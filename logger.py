8df9f1b97b5e8314787bdfd666bf24cde11f12ae
Moein01 <moeinkhan2002@gmail.com>
2025-07-25 13:10:09 +0430

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,6 +1,2 @@
-import logging
-
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
 def log(message):
-    logging.info(message)
+    print(f"[LOG] {message}")
